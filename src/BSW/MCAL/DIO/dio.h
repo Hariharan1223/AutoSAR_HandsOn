305c26f21ef73bb9b0faf134d61f24c45aba6611
Hariharan1223 <hariharanb1223@gmail.com>
2024-07-05 21:13:26 +0530

    dio driver header file

--- src/BSW/MCAL/DIO/dio.h
+++ src/BSW/MCAL/DIO/dio.h
@@ -1,21 +1,32 @@
+/**
+ * @file dio.h
+ * @brief dio driver header file
+ */
 #ifndef DIO_H
 #define DIO_H
 
 typedef unsigned int Dio_LevelType 
 typedef unsigned int Dio_ChannelType
+typedef unsigned int Std_ReturnType;
 
-#define STD_LOW	0x00  /* Physical state 0V */
-#define STD_HIGH 0x01 /* Physical state 5V or 3.3V */
+/* Physical state 0V */
+#define STD_LOW	0x00  
+ /* Physical state 5V or 3.3V */
+#define STD_HIGH 0x01
 
 #define GPIO_PORTA_PIN3	3
-#define GPIOA_BASE_ADDRESS	0x40020000  // Assuming the base address of GPIO port A
-#define GPIO_IDR (*((volatile uint32_t *)	(GPIOA_BASE_ADDRESS + 0x10))) /* Input Data Register */
-#define GPIO_ODR (*((volatile uint32_t *)	(GPIOA_BASE_ADDRESS + 0x14))) /* Output Data Register */
+/* Assuming the base address of GPIO port A */
+#define GPIOA_BASE_ADDRESS	0x40020000  
+/* Input Data Register */
+#define GPIO_IDR (*((volatile uint32_t *)	(GPIOA_BASE_ADDRESS + 0x10))) 
+/* Output Data Register */
+#define GPIO_ODR (*((volatile uint32_t *)	(GPIOA_BASE_ADDRESS + 0x14)))
 
 /* Error codes */
 #define DIO_E_PARAM_INVALID_CHANNEL_ID 0x01
 Dio_LevelType Dio_ReadChannel(Dio_ChannelType ChannelId);
 void Dio_WriteChannel(Dio_ChannelType ChannelId, Dio_LevelType Level);
+
 /* Function to report errors */
 void Dio_ReportError(uint8_t ErrorCode);
 
